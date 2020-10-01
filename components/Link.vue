5d9a321445d2e544bf9239cf44dc2eaf74d8417c
Sergey Maksimenko <serega.maksimenko@gmail.com>
2020-10-01 19:01:08 +0600

    initial

---
+++ components/Link.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- <nuxt-link to="/about">About page</nuxt-link> -->
+    <NuxtLink to="/about">About page</NuxtLink>
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
